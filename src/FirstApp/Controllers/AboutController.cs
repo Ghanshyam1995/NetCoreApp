f5cf4ebef81d14d6f730050fb8e2f4249365a831
Nihkil <nikhil.l@flatworldsolutions.com>
2017-06-28 20:33:38 +0530

    Initial Commit

---
+++ src/FirstApp/Controllers/AboutController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstApp.Controllers
+{
+    [Route("[controller]")]
+    public class AboutController : Controller
+    {
+        [Route("")]
+        public string Phone()
+        {
+            return "My Name is Ghanshyam Singh";
+        }
+        [Route("[action]")]
+        public string  Country()
+        {
+            return "India";
+        }
+    }
+}
